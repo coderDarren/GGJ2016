58dc5ec2def2fe6d4ea69c78365b0b075152588b
coderDarren <doneale3@gmail.com>
2016-01-30 17:56:29 -0500

    Menu Logic

---
+++ GGJ2016WinningGame/Assets/MenuMaker/Scripts/UI/UIButton_SetScene.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class UIButton_SetScene : MonoBehaviour, IPointerClickHandler {
+
+	public string scene = "ENTER SCENE HERE";
+	public string window_state = "ENTER WINDOW STATE HERE";
+    
+    WindowState state;
+
+    void Start()
+    {
+        state = GameObject.FindGameObjectWithTag("WindowManager").GetComponent<WindowState>();
+    }
+	
+    public void OnPointerClick(PointerEventData ped)
+    {
+        state.SetState(window_state);
+        SceneManager.LoadScene(scene);
+    }
+}
