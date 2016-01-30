083bbdea83872bce4a7cb0c96ed1e8a14c13ddd7
Jared Hill <jhill2@spsu.edu>
2016-01-30 08:45:55 -0500

    Turret targeting
    the turret can now targeting the player and looking directly at the
    players position.

---
+++ GGJ2016WinningGame/Assets/Scripts/AI/Turret/lookAtPlayer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lookAtPlayer : MonoBehaviour {
+
+	public bool playerSighted;
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (playerSighted)
+		{
+			transform.LookAt(player.transform);
+		}
+	}
+
+	void OnTriggerEnter(Collider coll)
+	{
+
+		if (coll.tag == "Player");
+		{
+			playerSighted = true;
+		}
+	}
+}
