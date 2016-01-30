f902a96f6d4918aaf84f50fdc3459a3b0b393c1b
coderDarren <doneale3@gmail.com>
2016-01-30 00:40:24 -0500

    Player Controller Stuff
    -Imported Animations
    -Began ControllerTesting scene
    -Initial player movement (without animation) is ready.

---
+++ GGJ2016WinningGame/Assets/Scripts/Controller/Character/PlayerAnimationState.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAnimationState : MonoBehaviour {
+
+	public enum PlayerState
+    {
+        STAND_IDLE,//0
+        STAND_WALK,//1
+        STAND_WALKBACK,//2
+        STAND_RUN,//3
+        STAND_RUNBACK,//4
+        STAND_STRAFELEFT,//5
+        STAND_STRAFERIGHT,//6
+        IDLE_JUMP,//7
+        RUN_JUMP,//8
+        CROUCH_IDLE,//9
+        CROUCH_WALK,//10
+        CROUCH_WALKBACK,//11
+        CROUCH_STRAFELEFT,//12
+        CROUCH_STRAFERIGHT,//13
+        STRANGLE,//14
+        THROW//15
+    }
+    public PlayerState playerState;
+
+    int state;
+
+    void Update()
+    {
+        state = (int)playerState;
+    }
+}
