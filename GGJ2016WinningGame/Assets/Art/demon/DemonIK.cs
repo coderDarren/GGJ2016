823785a7f0f25d2cf56d37ab71fb59378f9bc132
Boredman123 <boredman123@live.com>
2016-01-31 00:09:18 -0500

    Level Boss and Lighting changed
    lighting and boss

---
+++ GGJ2016WinningGame/Assets/Art/demon/DemonIK.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DemonIK : MonoBehaviour {
+
+	Animator anim;
+
+	public Transform player;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+
+	void OnAnimatorIK(){
+		anim.SetLookAtPosition(player.position);       
+		anim.SetLookAtWeight(1.0f);
+	}
+}
