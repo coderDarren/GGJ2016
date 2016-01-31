b9af4282e8acdcb28add3aeca1900a4be904d2bf
BitterBuffalo <tthefriddler@gmail.com>
2016-01-30 21:18:29 -0500

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    
    	GGJ2016WinningGame/Assets/Models/Atienza/Ely_by_K._Atienza@throw_object.fbm.meta
    
    	GGJ2016WinningGame/Assets/Models/Atienza/Ely_by_K._Atienza@throw_object.fbm/ely-vanguardsoldier-kerwinatienza_diffuse.png.meta
    
    	GGJ2016WinningGame/Assets/Models/Atienza/Ely_by_K._Atienza@throw_object.fbm/ely-vanguardsoldier-kerwinatienza_glow.png.meta
    
    	GGJ2016WinningGame/Assets/Models/Atienza/Ely_by_K._Atienza@throw_object.fbm/ely-vanguardsoldier-kerwinatienza_normal.png.meta
    
    	GGJ2016WinningGame/Assets/Models/Atienza/Ely_by_K._Atienza@throw_object.fbm/ely-vanguardsoldier-kerwinatienza_specular.png.meta

---
+++ GGJ2016WinningGame/Assets/Scripts/Ninja Staaaas.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NinjaStaaaas : MonoBehaviour {
+    public float rotationSpeed;
+    public float moveSpeed;
+    public Camera cam;
+    private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        transform.forward = cam.transform.forward;
+        rb.AddForce(cam.transform.forward * moveSpeed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Quaternion rotation = Quaternion.Euler(rb.transform.localEulerAngles * rotationSpeed * Time.deltaTime);
+        rb.MoveRotation(rb.rotation * rotation);
+	}
+}
