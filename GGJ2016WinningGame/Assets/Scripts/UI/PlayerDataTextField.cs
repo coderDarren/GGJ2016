a0fa273b3b0aa165fc0abed0f6ec9d98735f3db3
coderDarren <doneale3@gmail.com>
2016-01-30 20:43:27 -0500

    Level Menu Update

---
+++ GGJ2016WinningGame/Assets/Scripts/UI/PlayerDataTextField.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PlayerDataTextField : MonoBehaviour {
+
+    public enum PlayerDataType
+    {
+        Level,
+        SkillPoint
+        
+    }
+    public PlayerDataType playerDataType;
+
+    Text t;
+
+    void Start()
+    {
+        t = GetComponent<Text>();
+        switch (playerDataType)
+        {
+            case PlayerDataType.Level: break;
+            case PlayerDataType.SkillPoint: t.text = "Skill Points: " +PlayerData.Instance.skillPoints.ToString(); break;
+        }
+    }
+
+    void Update()
+    {
+        if (playerDataType == PlayerDataType.Level)
+            t.text = PlayerData.Instance.level.ToString();
+    }
+}
