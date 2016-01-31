155e50642ac8121f7342c586a88ed2ade1b0e789
BitterBuffalo <tthefriddler@gmail.com>
2016-01-31 01:59:11 -0500

    ForcePush and Ragdoll

---
+++ GGJ2016WinningGame/Assets/Scripts/ForcePush.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForcePush : MonoBehaviour {
+    public ParticleSystem system;
+    public Camera cam;
+    public float forceAmount;
+    private ParticleSystem.EmissionModule emission;
+
+    public float distance;
+    // Use this for initialization
+    void Start () {
+        //system = GetComponent<ParticleSystem>();
+        emission = system.emission;
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit hit;
+        if(Input.GetButtonDown("Force"))
+        {
+            emission.SetBursts(new ParticleSystem.Burst[] { new ParticleSystem.Burst(2.0f, 100) });
+
+            if (Physics.Raycast(transform.position, cam.transform.forward, out hit, distance))
+            {
+                if (hit.transform.gameObject.tag == "Enemy")
+                {
+                    GameObject ragdoll = Instantiate(Resources.Load("Ragdoll Vanguard"), hit.transform.position,
+                        Quaternion.identity) as GameObject;
+                    Destroy(hit.transform.gameObject);
+                    ragdoll.transform.Find("mixamorig:Hips").GetComponent<Rigidbody>().AddForce(cam.transform.forward * forceAmount, ForceMode.Impulse);
+                    //ragdoll.GetComponent<Rigidbody>().AddForce(cam.transform.forward * forceAmount);
+                    //foreach(Transform t in ragdoll.transform)
+                    //{
+                    //t.position += Vector3.forward * 5;
+                    //if(t.GetComponent<Rigidbody>() != null)
+                    //{
+                    //t.GetComponent<Rigidbody>().AddForce(cam.transform.forward * forceAmount, ForceMode.Impulse);
+                    //}
+                    //}
+                }
+            }
+        }
+    }
+}
