66792eb346e6640dec753df4e74f3bfb9b8deabf
Boredman123 <boredman123@live.com>
2016-01-31 11:16:30 -0500

    enemy health added

---
+++ GGJ2016WinningGame/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHealth : MonoBehaviour {
+
+	public int maxHealth = 100;
+	float explosionRagdollForce = 100;
+	int curHealth;
+	GameObject explosion;
+
+
+	void Start(){
+		curHealth = maxHealth;
+	}
+
+
+	public void TakeDmg(int dmg){
+		curHealth -= dmg;
+		if (curHealth <= 0) {
+			GameObject ragdoll = Instantiate(Resources.Load("Ragdoll Vanguard"), transform.position,
+				Quaternion.identity) as GameObject;
+			ragdoll.transform.Find("mixamorig:Hips").GetComponent<Rigidbody>().AddForce(Vector3.up * explosionRagdollForce, ForceMode.Impulse);
+			Instantiate (explosion);
+
+			Destroy (this.gameObject);
+		}
+
+	}
+
+}
