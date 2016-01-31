18acfa2901b08f4ce7994d5269fddf23da8e56e6
Jared Hill <jhill2@spsu.edu>
2016-01-31 11:32:15 -0500

    Adding music files and bullet prefabs

---
+++ GGJ2016WinningGame/Assets/Scripts/CollisionDestroy.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionDestroy : MonoBehaviour {
+
+	public GameObject explosion;
+
+	void OnCollision(Collision coll)
+	{
+		Destroy(this.gameObject);
+		Instantiate(explosion, coll.transform.position,coll.transform.rotation);
+	}
+}
