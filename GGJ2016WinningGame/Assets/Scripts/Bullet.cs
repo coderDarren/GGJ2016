00215596de47bc00114dfa6ca5f06c5a300c2199
Jared Hill <jhill2@spsu.edu>
2016-01-31 11:12:59 -0500

    working on the bullets and destroying the bullets

---
+++ GGJ2016WinningGame/Assets/Scripts/Bullet.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	public GameObject explosion;
+	MeshRenderer myBullet;
+	public float bulletTexRot;
+	// Use this for initialization
+	void Start () {
+		myBullet = GetComponent<MeshRenderer>();
+	}
+
+	void OnCollisionEnter(Collision coll)
+	{
+		Destroy(this.gameObject);
+		Instantiate(explosion, this.gameObject.transform.position, this.gameObject.transform.rotation);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		myBullet.material.mainTextureOffset = new Vector2(0, Time.time * bulletTexRot);
+	}
+}
---
+++ GGJ2016WinningGame/Assets/Scripts/TriggerDestroy.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerDestroy : MonoBehaviour {
+	public GameObject explosion;
+
+	void OnTriggerEnter(Collider coll)
+	{
+		Destroy(this.gameObject);
+		Instantiate(explosion, this.gameObject.transform.position, this.gameObject.transform.rotation);
+	}
+}
